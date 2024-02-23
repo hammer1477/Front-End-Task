703c8d1c4aec3c31d65f8235fd93249c0959403e
Nadim Nassar <48570832+hammer1477@users.noreply.github.com>
2024-02-23 17:39:25 +0200

    Add files via upload

---
+++ src/Components/Onseller.js
@@ -0,0 +1,17 @@
+import './product.css';
+
+const Onseller = () => {
+    return (
+        <div>
+            <div id='bestSellerZ'>
+            <img src="https://drive.google.com/file/d/1cAsxFkBL9vV--swjAmuiQKPOmdJkHOfV/view?usp=drive_link" alt='' className='image'></img>
+                <text className='Report'>Report for</text>
+            <div><text className='ReportS'>B  E  S  T</text></div>
+            <div><text className='ReportSS'>S  A  L  E  S</text></div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Onseller
